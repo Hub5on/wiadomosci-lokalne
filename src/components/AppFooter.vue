4bba7733d6435e48d17f32af7d7c07e97578180c
Hub5on <haibert.hs32@gmail.com>
2024-05-05 19:50:40 +0200

    init

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="footer bg-dark text-white text-center py-4">
+      <div class="container">
+        <span>© 2024 My Website. All rights reserved.</span>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AppFooter',
+    // Możesz tutaj zdefiniować właściwości i metody komponentu Footer
+  }
+  </script>
+  
+  <style scoped>
+  /* Stylizacja specyficzna dla komponentu Footer */
+  </style>
