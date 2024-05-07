e3bad540b54f2d53114c6513101c4dce479e51ff
Hub5on <haibert.hs32@gmail.com>
2024-05-07 17:54:31 +0200

    build(deps): update package-lock.json, package.json, App.vue and NavbarMobile.vue

--- src/components/NavbarMobile.vue
+++ src/components/NavbarMobile.vue
@@ -1,27 +1,22 @@
 <template>
 <div class="container fixed-bottom bg-light">
-  <!-- Ikony Navbaru -->
   <div class="d-flex justify-content-between align-items-center py-2">
-    <!-- Ikona Strony głównej -->
-    <div class="text-center mx-2">
-      <a href="#">
-        <i class="bi bi-house-door text-dark" style="font-size: 24px;"></i> <!-- Zwiększenie rozmiaru ikony -->
+    <div class="text-center mx-3">
+      <a href="/">
+        <i class="bi bi-house-door text-dark" style="font-size: 24px;"></i>
       </a>
     </div>
-    <!-- Ikona Profilu -->
     <div class="text-center mx-2">
       <a href="#">
         <i class="bi bi-calendar text-dark" style="font-size: 24px;"></i>
       </a>
     </div>
-    <!-- Ikona Wiadomości -->
     <div class="text-center mx-2">
       <a href="#">
         <i class="bi bi-bell text-dark" style="font-size: 24px;"></i>
       </a>
     </div>
-    <!-- Ikona Ustawień -->
-    <div class="text-center mx-2">
+    <div class="text-center mx-3">
       <a href="#">
         <i class="bi bi-gear text-dark" style="font-size: 24px;"></i>
       </a>
@@ -44,7 +39,7 @@
   
   <script>
   export default {
-    name: 'NavbarDesktop',
+    name: 'NavbarMobile',
   }
   </script>
   
