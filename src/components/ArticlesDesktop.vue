6a8fd6279fd971280afc2359a6cd41b28adb26ee
Hub5on <haibert.hs32@gmail.com>
2024-06-05 11:54:44 +0200

    refactor: Update App.vue to display ArticlesDesktop component for desktop users

---
+++ src/components/ArticlesDesktop.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+      <ul class="list-group list-group-flush p-3">
+        <li class="list-group-item list-group-item-action" v-for="article in articles" :key="article._id">
+          <h2 class="mb-3">{{ article.title }}</h2>
+          <p>{{ article.description }}</p>
+          <p>Autor: {{ article.creator }}</p>
+          <p>Data publikacji: {{ formatDateTime(article.pubDate) }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ArticleList',
+    data() {
+      return {
+        articles: []
+      };
+    },
+    created() {
+      this.fetchArticles();
+    },
+    methods: {
+      async fetchArticles() {
+        try {
+          const response = await fetch('/api/articles');
+          const data = await response.json();
+          // Sortowanie artykułów po dacie (od najnowszego do najstarszego)
+          this.articles = data.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+        } catch (error) {
+          console.error('Błąd pobierania artykułów:', error);
+        }
+      },
+      formatDateTime(dateTime) {
+        const date = new Date(dateTime);
+        const formattedDate = `${this.addZeroIfNeeded(date.getDate())}/${this.addZeroIfNeeded(date.getMonth() + 1)}/${date.getFullYear()}`;
+        const formattedTime = `${this.addZeroIfNeeded(date.getHours())}:${this.addZeroIfNeeded(date.getMinutes())}`;
+        return `${formattedDate} ${formattedTime}`;
+      },
+  
+      addZeroIfNeeded(num) {
+        return num < 10 ? '0' + num : num;
+      }
+    }
+  };
+  </script>
+  
