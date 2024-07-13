06927c96603be0524d9e95cfb10316dfbc5d195b
Hubert <haibert.hs32@gmail.com>
2024-07-13 22:30:40 +0200

    Revert "refactor: Update ArticlesDesktop.vue layout and functionality"

--- src/components/ArticlesMobile.vue
+++ src/components/ArticlesMobile.vue
@@ -1,22 +1,11 @@
 <template>
   <div>
     <ul class="list-group list-group-flush p-3">
-      <li
-        class="list-group-item list-group-item-action article-item"
-        v-for="article in articles"
-        :key="article._id"
-        @click="goToSource(article.link)"
-        style="cursor: pointer;"
-      >
-        <img :src="article.imageUrl" alt="Article Image" v-if="article.imageUrl" class="img-fluid">
-        <div class="article-text p-3">
-          
-            <div class="col-md-6">
-              <p class="pub-date"><strong>Data publikacji:</strong> {{ formatDateTime(article.pubDate) }}</p>
-            </div>
-          <h2 class="mb-3">{{ article.title }}</h2>
-          <p>{{ article.description }}</p>
-        </div>
+      <li class="list-group-item list-group-item-action" v-for="article in articles" :key="article._id">
+        <h2 class="mb-3">{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+        <p>Autor: {{ article.creator }}</p>
+        <p>Data publikacji: {{ formatDateTime(article.pubDate) }}</p>
       </li>
     </ul>
   </div>
@@ -36,45 +25,13 @@ export default {
   methods: {
     async fetchArticles() {
       try {
-        const response = await fetch('http://localhost:3000/api/articles');
+        const response = await fetch('/api/articles');
         const data = await response.json();
+        // Sortowanie artykułów po dacie (od najnowszego do najstarszego)
         this.articles = data.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
-
-        await Promise.all(this.articles.map(async (article) => {
-          const imageUrl = await this.fetchFirstImage(article.link);
-          article.imageUrl = this.replaceLocalhostWithDomain(imageUrl);
-        }));
       } catch (error) {
         console.error('Błąd pobierania artykułów:', error);
       }
-    },
-    getBaseUrl() {
-      return window.location.origin;
-    },
-    async fetchFirstImage(link) {
-      try {
-        const response = await fetch(`http://localhost:3000/api/proxy?url=${encodeURIComponent(link)}`);
-        const html = await response.text();
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const imgElement = doc.querySelector('.container-subpage img');
-
-        if (imgElement) {
-          return imgElement.src;
-        } else {
-          return '';
-        }
-      } catch (error) {
-        console.error('Błąd pobierania obrazka:', error);
-        return '';
-      }
-    },
-    replaceLocalhostWithDomain(url) {
-      const baseUrl = this.getBaseUrl();
-      return url.replace(baseUrl, 'http://powiatsredzki.pl');
-    },
-    goToSource(link) {
-      window.location.href = link;
     },
     formatDateTime(dateTime) {
       const date = new Date(dateTime);
@@ -82,34 +39,10 @@ export default {
       const formattedTime = `${this.addZeroIfNeeded(date.getHours())}:${this.addZeroIfNeeded(date.getMinutes())}`;
       return `${formattedDate} ${formattedTime}`;
     },
+
     addZeroIfNeeded(num) {
       return num < 10 ? '0' + num : num;
     }
   }
 };
 </script>
-<style scoped>
-  .content {
-    margin-top: 40px;
-    padding: 0 25%; /* Dostosuj padding dla responsywności */
-    background-color: #f0f0f0;
-    border-radius: 15px;
-  }
-  .one-article li {
-    padding: 0;
-    margin-bottom: 25px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    background-color: #fff;
-    border-radius: 15px; /* Dodaj border-radius dla całego li */
-    overflow: hidden; /* Upewnij się, że obrazki wypełniają zaokrąglone rogi */
-  }
-  .article-item img {
-    width: 100%;
-    height: auto;
-    max-height: 20rem;
-  }
-  .article-item {
-    border-radius: 15px; /* Zaokrąglij rogi kontenera .article-item */
-  }
-  
-</style>
