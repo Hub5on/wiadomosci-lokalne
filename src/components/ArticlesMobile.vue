7cb3cd975b8025e0f8c794a1b0974d744bd208eb
Hub5on <haibert.hs32@gmail.com>
2024-07-15 12:09:12 +0200

    refactor: Improve ArticlesMobile.vue layout and functionality

--- src/components/ArticlesMobile.vue
+++ src/components/ArticlesMobile.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <ul class="list-group list-group-flush p-3">
-      <li class="list-group-item list-group-item-action" v-for="article in articles" :key="article._id">
-        <h2 class="mb-3">{{ article.title }}</h2>
-        <p>{{ article.description }}</p>
-        <p>Autor: {{ article.creator }}</p>
-        <p>Data publikacji: {{ formatDateTime(article.pubDate) }}</p>
+      <li
+        class="list-group-item list-group-item-action article-item"
+        v-for="article in articles"
+        :key="article._id"
+        @click="goToSource(article.link)"
+        style="cursor: pointer;"
+      >
+      <img :src="article.imageUrl" alt="Article Image" v-if="article.imageUrl" class="img-fluid">
+        <div class="article-text p-3">
+          <div class="row">
+            <div class="col-md-6">
+              <p class="pub-date"><strong>Data publikacji:</strong> {{ formatDateTime(article.pubDate) }}</p>
+            </div>
+            <div class="col-md-6">
+              <p class="author"><strong>Autor:</strong> {{ article.creator }}</p>
+            </div>
+          </div>
+          <h2 class="mb-3">{{ article.title }}</h2>
+          <p>{{ article.description }}</p>
+        </div>
       </li>
     </ul>
   </div>
@@ -25,13 +40,47 @@ export default {
   methods: {
     async fetchArticles() {
       try {
-        const response = await fetch('/api/articles');
+        const response = await fetch(`${this.getBaseUrl()}/api/articles`);
         const data = await response.json();
-        // Sortowanie artykułów po dacie (od najnowszego do najstarszego)
         this.articles = data.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+
+        await Promise.all(this.articles.map(async (article) => {
+          const imageUrl = await this.fetchFirstImage(article.link);
+          article.imageUrl = this.replaceLocalhostWithDomain(imageUrl);
+        }));
       } catch (error) {
         console.error('Błąd pobierania artykułów:', error);
       }
+    },
+    getBaseUrl() {
+      return window.location.origin;
+    },
+    async fetchFirstImage(link) {
+      try {
+        const response = await fetch(`${this.getBaseUrl()}/api/proxy?url=${encodeURIComponent(link)}`);
+        const html = await response.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const imgElement = doc.querySelector('.container-subpage img');
+
+        if (imgElement) {
+          return imgElement.src;
+        } else {
+          return '';
+        }
+      } catch (error) {
+        console.error('Błąd pobierania obrazka:', error);
+        return '';
+      }
+    },
+    replaceLocalhostWithDomain(url) {
+      const newDomain = 'powiatsredzki.pl';
+      const urlObj = new URL(url);
+      urlObj.hostname = newDomain;
+      return urlObj.toString();
+    },
+    goToSource(link) {
+      window.location.href = link;
     },
     formatDateTime(dateTime) {
       const date = new Date(dateTime);
@@ -39,7 +88,6 @@ export default {
       const formattedTime = `${this.addZeroIfNeeded(date.getHours())}:${this.addZeroIfNeeded(date.getMinutes())}`;
       return `${formattedDate} ${formattedTime}`;
     },
-
     addZeroIfNeeded(num) {
       return num < 10 ? '0' + num : num;
     }
