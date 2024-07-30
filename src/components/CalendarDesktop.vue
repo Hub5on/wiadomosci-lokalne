bdc04793df2b35f8b898b75324f69baf2de2e041
Hub5on <haibert.hs32@gmail.com>
2024-07-30 18:44:45 +0200

    feat: Add FullCalendar components and update CalendarDesktop.vue
    - Added @fullcalendar/core, @fullcalendar/daygrid, @fullcalendar/interaction, and @fullcalendar/vue3 dependencies
    - Imported FullCalendar component and plugins in CalendarDesktop.vue
    - Updated CalendarDesktop.vue template to use FullCalendar component
    - Updated CalendarDesktop.vue style to customize calendar appearance

--- src/components/CalendarDesktop.vue
+++ src/components/CalendarDesktop.vue
@@ -1,17 +1,113 @@
 <template>
-    <div>
-      <h1>Calendar Desktop</h1>
+  <div class="container mt-5">
+    <div class="calendar-container">
+      <FullCalendar :options="calendarOptions" />
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'CalendarDesktop'
+  </div>
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import plLocale from '@fullcalendar/core/locales/pl'
+
+export default {
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        events: [
+          { title: 'event 1', start: '2024-08-01', end: '2024-08-03' },
+          { title: 'event 2', date: '2024-08-02' }
+        ],
+        headerToolbar: {
+          left: 'prev,next',
+          center: 'title',
+          right: 'today'
+        },
+        buttonText: {
+          today: 'Dzisiaj',
+          month: 'Miesiąc',
+          week: 'Tydzień',
+          day: 'Dzień'
+        },
+        locale: 'pl',
+        locales: [plLocale],
+        /*weekNumbers: true,
+          weekNumberFormat: {
+          week: 'numeric'
+        }*/
+      }
+    }
+  },
+  methods: {
+    handleDateClick(arg) {
+      alert('date click! ' + arg.dateStr)
+    }
   }
-  </script>
-  <style>
-  h1:first-child {
-    margin-top: 70px;
-  }
-  </style>
-  
+}
+</script>
+
+<style>
+.calendar-container {
+  margin: 0 auto;
+  padding: 30px 40px 0 40px;
+  max-width: 1000px;
+  font-family: 'Lato', sans-serif;
+}
+
+.fc-daygrid-day-top {
+  font-weight: bold;
+}
+
+.fc-daygrid-day-number {
+  font-size: 1.2em;
+}
+
+.fc-daygrid-day-content {
+  font-size: 0.9em;
+}
+
+.fc-toolbar-title {
+  font-size: 1.5em;
+  font-weight: 700;
+}
+
+.fc-button {
+  font-size: 0.9em;
+  border-radius: 4px;
+}
+
+.fc-button-primary {
+  border: none;
+  color: #fff;
+}
+
+.fc-button-primary:hover {
+  background-color: #0056b3;
+}
+
+.fc-daygrid-day-number {
+  color: #000;
+  text-decoration: none;
+}
+
+.fc-daygrid-day-content {
+  color: #000;
+  text-decoration: none;
+}
+
+.fc-col-header-cell-cushion {
+  text-decoration: none;
+  color: #000;
+}
+.fc-daygrid-week-number{
+  text-decoration: none;
+}
+</style>
