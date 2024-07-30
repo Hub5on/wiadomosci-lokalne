a6f017123c8838bbb2cc61410b7a62b21ca0ddae
Hub5on <haibert.hs32@gmail.com>
2024-07-30 17:10:15 +0200

    feat: Add CalendarMobile and CalendarDesktop components
    - Added CalendarMobile.vue component
    - Added CalendarDesktop.vue component
    - Updated router/index.js to include CalendarPage route
    - Updated main.js to import and use router
    - Updated App.vue to render CalendarPage component based on route
    - Added CalendarPage.vue component to render CalendarDesktop or CalendarMobile based on window width

---
+++ src/components/CalendarPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <CalendarDesktop v-if="isDesktop"/>
+    <CalendarMobile v-else/>
+  </div>
+</template>
+
+<script>
+import CalendarDesktop from './CalendarDesktop.vue';
+import CalendarMobile from './CalendarMobile.vue';
+
+export default {
+  name: 'CalendarPage',
+  data() {
+    return {
+      isDesktop: window.innerWidth >= 768
+    };
+  },
+  components: {
+    CalendarDesktop,
+    CalendarMobile
+  },
+  mounted() {
+    window.addEventListener('resize', this.handleResize);
+  },
+  unmounted() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.isDesktop = window.innerWidth >= 768;
+    }
+  }
+}
+</script>
