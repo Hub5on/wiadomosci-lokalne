28ee179ac1f91ae31e912b2238149852a3cc4c7b
Hub5on <haibert.hs32@gmail.com>
2024-07-30 19:40:52 +0200

    refactor: Update FullCalendar component and styles in CalendarMobile.vue and NavbarMobile.vue

--- src/components/NavbarMobile.vue
+++ src/components/NavbarMobile.vue
@@ -1,49 +1,56 @@
 <template>
-<div class="container fixed-bottom bg-light">
-  <div class="d-flex justify-content-between align-items-center py-2">
-    <div class="text-center mx-3">
-      <a href="/">
-        <i class="bi bi-house-door text-dark" style="font-size: 24px;"></i>
-      </a>
-    </div>
-    <div class="text-center mx-2">
-      <a href="#">
-        <i class="bi bi-calendar text-dark" style="font-size: 24px;"></i>
-      </a>
-    </div>
-    <div class="text-center mx-2">
-      <a href="#">
-        <i class="bi bi-bell text-dark" style="font-size: 24px;"></i>
-      </a>
-    </div>
-    <div class="text-center mx-3">
-      <a href="#">
-        <i class="bi bi-gear text-dark" style="font-size: 24px;"></i>
-      </a>
+  <div class="navbar-mobile fixed-bottom bg-light">
+    <div class="d-flex justify-content-between align-items-center py-2">
+      <div class="text-center px-2">
+        <router-link to="/">
+          <i class="bi bi-house-door text-dark" style="font-size: 24px;"></i>
+        </router-link>
+      </div>
+      <div class="text-center px-2">
+        <router-link to="/calendar">
+          <i class="bi bi-calendar text-dark" style="font-size: 24px;"></i>
+        </router-link>
+      </div>
+      <div class="text-center px-2">
+        <router-link to="/notifications">
+          <i class="bi bi-bell text-dark" style="font-size: 24px;"></i>
+        </router-link>
+      </div>
+      <div class="text-center px-2">
+        <router-link to="/settings">
+          <i class="bi bi-gear text-dark" style="font-size: 24px;"></i>
+        </router-link>
+      </div>
     </div>
   </div>
-</div>
+</template>
 
+<script>
+export default {
+  name: 'NavbarMobile',
+}
+</script>
 
+<style scoped>
+.navbar-mobile {
+  width: 100%;
+  box-sizing: border-box;
+}
 
+.navbar-mobile .d-flex {
+  width: 100%;
+}
 
+.text-center {
+  flex: 1;
+}
 
+.text-center > a {
+  display: block;
+  padding: 0.5rem; /* Adjust this value as needed */
+}
 
-
-
-
-
-
-
-  </template>
-  
-  <script>
-  export default {
-    name: 'NavbarMobile',
-  }
-  </script>
-  
-  <style scoped>
-  /* Stylizacja specyficzna dla komponentu Navbar */
-  </style>
-  
+.text-center > a i {
+  font-size: 24px;
+}
+</style>
