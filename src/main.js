8732fe84c7fb8c3c08838a927387309eeecf8382
Hub5on <haibert.hs32@gmail.com>
2024-08-16 14:37:37 +0200

    refactor: Update vue.config.js and package.json
    - Update devServer port to 3001 in vue.config.js
    - Add vue-lazyload dependency in package.json
    - Add wiadomosci-lokalne dependency in package.json
    - Remove unused code in api/index.js
    - Add /api/proxy endpoint in api/index.js
    - Add request logging middleware in api/index.js
    - Update package-lock.json with new dependencies

--- src/main.js
+++ src/main.js
@@ -5,9 +5,19 @@ import './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import VueLazyload from 'vue-lazyload';
+
 
 const app = createApp(App);
 
-app.use(router); // Użyj routera
 
+app.use(VueLazyload, {
+  loading: '/img/loading.gif', 
+  error: '/img/error.png' 
+});
+
+// Użyj routera
+app.use(router);
+
+// Zamontuj aplikację
 app.mount('#app');
