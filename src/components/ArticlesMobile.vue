27910629f5e58ce613783b033db615f2d824981b
Hub5on <haibert.hs32@gmail.com>
2024-10-03 17:42:39 +0200

    refactor: Improve image processing and handling of deleted articles in ArticlesMobile.vue
    - Refactored the image processing and handling logic in the ArticlesMobile.vue component to improve performance and handle deleted articles more effectively.
    - Updated the rendering of article items to display a loading image while the article image is being fetched.
    - Added error handling for cases where the article image is not available or the page is deleted.
    - Improved the layout of the article text, including the publication date and author information.
    
    fix: Update navigation links in NavbarMobile.vue
    
    - Updated the navigation links in the NavbarMobile.vue component to use Polish translations for "kalendarz" (calendar), "powiadomienia" (notifications), and "ustawienia" (settings).

--- src/components/ArticlesMobile.vue
+++ src/components/ArticlesMobile.vue
@@ -1,33 +1,31 @@
 <template>
   <div>
     <ul class="list-group list-group-flush p-3">
-      <li
-        class="list-group-item list-group-item-action article-item"
-        v-for="article in articles"
-        :key="article._id"
-        @click="goToSource(article.link)"
-        style="cursor: pointer;"
-      >
-        <!-- Placeholder podczas ładowania -->
-        <img v-lazy="article.loadingImageUrl" alt="Loading Image" v-if="!article.imageUrl && !article.imageError" class="loading-image">
-        <!-- Obrazek artykułu -->
-        <img v-lazy="article.imageUrl" alt="Article Image" v-if="article.imageUrl" :class="article.imageClass">
-        <!-- Komunikat o błędzie -->
-        <div v-if="article.imageError" class="no-image-warning">
-          <img v-lazy="'img/error.png'" alt="Error Image" class="error-image">
+      <li class="list-group-item list-group-item-action article-item rounded-3" v-for="article in articles"
+        :key="article._id" @mousedown="handleMouseDown(article, $event)" style="cursor: pointer;">
+        <img v-lazy="article.loadingImageUrl" alt="Loading Image" v-if="article.isLoading" class="loading-image">
+        <div v-if="!article.isPageDeleted && !article.imageError">
+          <img v-lazy="article.imageUrl" alt="Article Image" :class="article.imageClass">
+        </div>
+        <div v-else class="no-image-warning">
+          <img v-lazy="'/img/error.png'" alt="Error Image" class="error-image">
+          <span class="text-danger"><strong>Strona usunięta</strong></span>
         </div>
         <div class="article-text p-3">
-          <div class="d-flex justify-content-between align-items-center mb-2">
-            <p class="pub-date m-0">
-              <strong>Data publikacji:</strong> {{ formatDateTime(article.pubDate) }}
-              <!-- Czerwony napis jeśli brak zdjęcia -->
-              <span v-if="article.imageError" class="text-danger"><strong>Strona usunięta</strong></span>
-            </p>
-            <p class="author m-0"><strong>Autor:</strong> {{ article.creator }}</p>
+          <div class="row">
+            <div class="col-md-6">
+              <p class="pub-date">
+                <strong>Data publikacji:</strong> {{ formatDateTime(article.pubDate) }}
+              </p>
+            </div>
+            <div class="col-md-6">
+              <p class="author"><strong>Autor:</strong> {{ article.creator }}</p>
+            </div>
           </div>
           <h2 class="mb-3">{{ article.title }}</h2>
           <p>{{ article.description }}</p>
         </div>
+
       </li>
     </ul>
   </div>
@@ -47,111 +45,159 @@ export default {
   methods: {
     async fetchArticles() {
       try {
+        await this.scrapeRss();
         const response = await fetch(`${this.getBaseUrl()}/api/articles`);
         const data = await response.json();
-        this.articles = data.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
 
-        // Przetwarzanie obrazków
-        await Promise.all(this.articles.map(async (article) => {
-          article.loadingImageUrl = '/img/loading.gif'; // Placeholder image URL
-          const imageUrl = await this.fetchFirstImage(article.link);
-          const { url, className } = await this.processImage(imageUrl);
+        this.articles = data.sort((a, b) => {
+          const dateComparison = new Date(b.pubDate) - new Date(a.pubDate);
+          return dateComparison !== 0 ? dateComparison : b.title.localeCompare(a.title);
+        });
 
-          // Tylko jeśli obrazek jest poprawny, przypisz URL i klasę
-          if (url) {
-            article.imageUrl = this.replaceLocalhostWithDomain(url);
-            article.imageClass = className;
-            article.imageError = false;
-          } else {
-            // W przeciwnym razie ustaw URL i klasę jako puste
-            article.imageUrl = '';
-            article.imageClass = '';
-            article.imageError = true;
-          }
-        }));
-
+        await Promise.all(this.articles.map(this.processArticle));
       } catch (error) {
         console.error('Błąd pobierania artykułów:', error);
       }
     },
+
+    async processArticle(article) {
+      article.loadingImageUrl = '/img/loading.gif';
+      article.isLoading = true;
+
+      const { imageUrl, isPageDeleted, originalLinkUsed } = await this.fetchFirstImage(article.link);
+      const { url, className } = await this.processImage(imageUrl);
+
+      if (isPageDeleted) {
+        article.isPageDeleted = true;
+        article.imageUrl = '/img/error.png';
+        article.imageClass = 'img-wide';
+        article.imageError = true;
+        article.redirectLink = originalLinkUsed;
+      } else if (!url) {
+        article.imageUrl = '/img/temp.jpg';
+        article.imageClass = 'img-wide';
+        article.imageError = false;
+        article.redirectLink = article.link;
+      } else {
+        article.imageUrl = url;
+        article.imageClass = className;
+        article.imageError = false;
+        article.redirectLink = originalLinkUsed;
+      }
+
+      article.isLoading = false;
+    },
+
+    async fetchFirstImage(link) {
+      try {
+        const html = await this.fetchHtml(link);
+        let isPageDeleted = this.isPageDeleted(html);
+        let originalLinkUsed = link;
+
+        if (isPageDeleted) {
+          const archivedLink = link.replace('/aktualnosci/', '/archiwum-aktualnosci/');
+          const archiveHtml = await this.fetchHtml(archivedLink);
+          isPageDeleted = this.isPageDeleted(archiveHtml);
+
+          if (!isPageDeleted) {
+            originalLinkUsed = archivedLink;
+            return this.extractImage(archiveHtml, originalLinkUsed, isPageDeleted);
+          }
+        } else {
+          return this.extractImage(html, originalLinkUsed, isPageDeleted);
+        }
+
+        return { imageUrl: '', isPageDeleted: true, originalLinkUsed };
+      } catch (error) {
+        console.error('Error fetching image:', error);
+        return { imageUrl: '', isPageDeleted: true, originalLinkUsed: '' };
+      }
+    },
+
+    async fetchHtml(url) {
+      const response = await fetch(`${this.getBaseUrl()}/api/proxy?url=${encodeURIComponent(url)}`);
+      return response.text();
+    },
+
+    isPageDeleted(html) {
+      return html.trim() === '<html><head></head><body></body></html>' || html.trim() === '';
+    },
+
+    extractImage(html, originalLinkUsed, isPageDeleted) {
+      const doc = new DOMParser().parseFromString(html, 'text/html');
+      const imgElement = doc.querySelector('.container-subpage img');
+      const imageUrl = imgElement ? imgElement.src : '';
+      return { imageUrl, isPageDeleted, originalLinkUsed };
+    },
+
+    async scrapeRss() {
+      try {
+        const response = await fetch(`${this.getBaseUrl()}/api/scrape-rss`);
+        if (!response.ok) {
+          throw new Error('Nie udało się pobrać danych RSS');
+        }
+        const result = await response.json();
+        console.log('Dane RSS:', result);
+      } catch (error) {
+        console.error('Błąd pobierania danych RSS:', error);
+      }
+    },
+
     getBaseUrl() {
       return window.location.origin;
     },
-    async fetchFirstImage(link) {
-      try {
-        const response = await fetch(`${this.getBaseUrl()}/api/proxy?url=${encodeURIComponent(link)}`);
-        const html = await response.text();
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const imgElement = doc.querySelector('.container-subpage img');
 
-        if (imgElement) {
-          return imgElement.src;
-        } else {
-          return '';
-        }
-      } catch (error) {
-        console.error('Error fetching image:', error);
-        return '';
-      }
-    },
     async processImage(imageUrl) {
-  if (!imageUrl) return { url: '', className: '' };
-  const replacedUrl = this.replaceLocalhostWithDomain(imageUrl);
+      if (!imageUrl) return { url: '', className: '' };
+      const replacedUrl = this.replaceLocalhostWithDomain(imageUrl);
 
-  return new Promise((resolve) => {
-    const img = new Image();
-    img.src = replacedUrl;
-    img.onload = () => {
-      const width = img.width;
-      const height = img.height;
-      let className = '';
+      const img = new Image();
+      img.src = replacedUrl;
 
-      if (width === height || height > width) {
-        className = 'img-square-or-tall';
-      } else {
-        className = 'img-wide';
-      }
-
-      resolve({ url: replacedUrl, className });
-    };
-    img.onerror = () => {
-      console.error('Error loading image:', replacedUrl); 
-      resolve({ url: replacedUrl, className: 'img-error' });
-    };
-  });
-}
-,
-    replaceLocalhostWithDomain(url) {
-  const targetDomain = 'powiatsredzki.pl'; 
-  let newUrl = url;
-  if (url.includes(window.location.hostname)) {
-    newUrl = url.replace(window.location.hostname, targetDomain);
-  }
-
-  // Tworzenie URL obiektu, aby manipulować jego elementami
-  const urlObj = new URL(newUrl);
-
-  // Usuń port, jeśli istnieje
-  urlObj.port = '';
-
-  const finalUrl = urlObj.toString();
-  
-
-  return finalUrl;
-},
-    goToSource(link) {
-      window.location.href = link;
+      return new Promise((resolve) => {
+        img.onload = () => {
+          const className = img.width === img.height || img.height > img.width ? 'img-square-or-tall' : 'img-wide';
+          resolve({ url: replacedUrl, className });
+        };
+        img.onerror = () => {
+          console.error('Error loading image:', replacedUrl);
+          resolve({ url: '', className: 'img-error' });
+        };
+      });
     },
+
+    replaceLocalhostWithDomain(url) {
+      const targetDomain = 'powiatsredzki.pl';
+      let newUrl = url.includes(window.location.hostname) ? url.replace(window.location.hostname, targetDomain) : url;
+
+      const urlObj = new URL(newUrl);
+      urlObj.port = '';
+
+      return urlObj.toString();
+    },
+
+    handleMouseDown(article, event) {
+      const link = article.redirectLink;
+      console.log("Redirecting to:", link);
+
+      if (event.button === 0) {
+        window.location.href = link;
+      } else if (event.button === 1) {
+        window.open(link, '_blank');
+        event.preventDefault();
+      }
+    },
+
     formatDateTime(dateTime) {
       const date = new Date(dateTime);
       const formattedDate = `${this.addZeroIfNeeded(date.getDate())}/${this.addZeroIfNeeded(date.getMonth() + 1)}/${date.getFullYear()}`;
       const formattedTime = `${this.addZeroIfNeeded(date.getHours())}:${this.addZeroIfNeeded(date.getMinutes())}`;
       return `${formattedDate} ${formattedTime}`;
     },
+
     addZeroIfNeeded(num) {
       return num < 10 ? '0' + num : num;
-    }
+    },
   }
 };
 </script>
