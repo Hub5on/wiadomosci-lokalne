cbc4db4f3d36bd9d0da96321b40fbf3eaf2041df
Hub5on <haibert.hs32@gmail.com>
2024-11-17 18:35:40 +0100

    Add weather API endpoint and refactor fetchWeather function to use it

--- src/WeatherService.js
+++ src/WeatherService.js
@@ -1,16 +1,14 @@
 export async function fetchWeather(locationName) {
-  const WEATHER_API = process.env.WEATHER_API;
-  console.log('WEATHER_API:', WEATHER_API);
-    try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&lang=pl&units=metric&appid=${WEATHER_API}`);
-      const data = await response.json();
-      return {
-        temp: data.main.temp,
-        description: data.weather[0].description,
-        icon: data.weather[0].icon
-      };
-    } catch (error) {
-      console.error('Błąd pobierania danych o pogodzie:', error);
-      return null;
-    }
+  try {
+    const response = await fetch(`/api/get-weather?locationName=${locationName}`);
+    const data = await response.json();
+    return {
+      temp: data.temp,
+      description: data.description,
+      icon: data.icon
+    };
+  } catch (error) {
+    console.error('Błąd pobierania danych o pogodzie:', error);
+    return null;
   }
+}
